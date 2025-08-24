fd915a914eae3e52750aa81840311bb56a29690f
JEEVANANDHAM31 <padmavathisaravanan769@gmail.com>
2025-08-24 12:38:13 +0530

    Set up done

--- src/api/fileUploadService.js
+++ src/api/fileUploadService.js
@@ -1,5 +1,7 @@
 import { useAuth } from '../context/AuthContext';
 
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
 const useFileUpload = () => {
   const { getAuthHeaders } = useAuth();
 
@@ -7,7 +9,7 @@ const useFileUpload = () => {
     const formData = new FormData();
     formData.append('file', file);
 
-    const response = await fetch(`http://localhost:8080/api/projects/${projectId}/files/upload`, {
+    const response = await fetch(`${API_BASE_URL}/projects/${projectId}/files/upload`, {
       method: 'POST',
       headers: getAuthHeaders(true), // Pass true to exclude Content-Type for FormData
       body: formData,
